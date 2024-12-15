3cd0f77eb1a3936a91f67df85f0a9749867f71c2
David Trestini <david.trestini@gmail.com>
2024-12-15 21:07:21 +0000

    change tree structure

--- generate_tree.sh
+++ generate_tree.sh
@@ -3,5 +3,5 @@
 #File: tree-md
 
 tree=$(tree -d -tf --noreport -I '*~' --charset ascii $1 |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' -e 's/\.\//https:\/\/github.com\/davidtrestini\/PNpedia\/tree\/main\//g'  -e ':loop' -e 's/(\(.*\) \(.*\))/(\1%20\2)/g' -e 't loop')
+       sed    -e 's:\.\/:\/:g' -e 's:\/.*\/:\/:g' -e 's:\/::g')
 echo "${tree}" > "./generated_tree.md"
